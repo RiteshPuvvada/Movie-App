45799fa526e84035355dd1ecfd24f7fabd9070f4
RiteshPuvvada <riteshpuvvada@gmail.com>
2022-05-10 10:08:12 +0530

    updated movie posters

--- src/Filter.js
+++ src/Filter.js
@@ -1,69 +1,68 @@
 import './App.css'
 import { useEffect } from "react";
 
-
-function Filter({setActiveGenre, activeGenre, setFiltered, popular}){
+function Filter({ setActiveGenre, activeGenre, setFiltered, popular }) {
 
     useEffect(() => {
-        if (activeGenre === 0){
+        if (activeGenre === 0) {
             setFiltered(popular);
             return;
         }
-        const filtered = popular.filter((movie) => 
-        movie.genre_ids.includes(activeGenre)
+        const filtered = popular.filter((movie) =>
+            movie.genre_ids.includes(activeGenre)
         );
         setFiltered(filtered);
-    },[activeGenre]);
+    }, [activeGenre]);
 
-    return(
+    return (
         <div >
-            <button className={activeGenre === 0 ? "active" : ""}  
-            className='bn30' 
-            onClick={() => setActiveGenre(0)}>All</button>
+            <button className={activeGenre === 0 ? "active" : ""}
+                className='btn'
+                onClick={() => setActiveGenre(0)}>All</button>
 
-             <button className={activeGenre === 28 ? "active" : ""}  
-            className='bn30' 
-            onClick={() => setActiveGenre(28)}>Action</button>
+            <button className={activeGenre === 28 ? "active" : ""}
+                className='btn'
+                onClick={() => setActiveGenre(28)}>Action</button>
 
-            <button className={activeGenre === 12 ? "active" : ""}  
-            className='bn30' 
-            onClick={() => setActiveGenre(12)}>Adventure</button>
+            <button className={activeGenre === 12 ? "active" : ""}
+                className='btn'
+                onClick={() => setActiveGenre(12)}>Adventure</button>
 
-            <button className={activeGenre === 16 ? "active" : ""}  
-            className='bn30' 
-            onClick={() => setActiveGenre(16)}>Animation</button>
+            <button className={activeGenre === 16 ? "active" : ""}
+                className='btn'
+                onClick={() => setActiveGenre(16)}>Animation</button>
 
-            <button className={activeGenre === 35 ? "active" : ""}  
-            className='bn30' 
-            onClick={() => setActiveGenre(35)}>Comedy</button>
+            <button className={activeGenre === 35 ? "active" : ""}
+                className='btn'
+                onClick={() => setActiveGenre(35)}>Comedy</button>
 
-            <button className={activeGenre === 80 ? "active" : ""}  
-            className='bn30' 
-            onClick={() => setActiveGenre(80)}>Crime</button>
-            
-            <button className={activeGenre === 18 ? "active" : ""}  
-            className='bn30' 
-            onClick={() => setActiveGenre(18)}>Drama</button>
-            
-            <button className={activeGenre === 14 ? "active" : ""}  
-            className='bn30' 
-            onClick={() => setActiveGenre(14)}>Fantasy</button>
+            <button className={activeGenre === 80 ? "active" : ""}
+                className='btn'
+                onClick={() => setActiveGenre(80)}>Crime</button>
 
-            <button className={activeGenre === 27 ? "active" : ""}  
-            className='bn30' 
-            onClick={() => setActiveGenre(27)}>Horror</button>
+            <button className={activeGenre === 18 ? "active" : ""}
+                className='btn'
+                onClick={() => setActiveGenre(18)}>Drama</button>
 
-            <button className={activeGenre === 9648 ? "active" : ""}  
-            className='bn30' 
-            onClick={() => setActiveGenre(9648)}>Mystery</button>
+            <button className={activeGenre === 14 ? "active" : ""}
+                className='btn'
+                onClick={() => setActiveGenre(14)}>Fantasy</button>
 
-            <button className={activeGenre === 878 ? "active" : ""}  
-            className='bn30' 
-            onClick={() => setActiveGenre(878)}>Science Fiction</button>
+            <button className={activeGenre === 27 ? "active" : ""}
+                className='btn'
+                onClick={() => setActiveGenre(27)}>Horror</button>
 
-            <button className={activeGenre === 53 ? "active" : ""}  
-            className='bn30' 
-            onClick={() => setActiveGenre(53)}>Thriller</button>
+            <button className={activeGenre === 9648 ? "active" : ""}
+                className='btn'
+                onClick={() => setActiveGenre(9648)}>Mystery</button>
+
+            <button className={activeGenre === 878 ? "active" : ""}
+                className='btn'
+                onClick={() => setActiveGenre(878)}>Science Fiction</button>
+
+            <button className={activeGenre === 53 ? "active" : ""}
+                className='btn'
+                onClick={() => setActiveGenre(53)}>Thriller</button>
         </div>
     )
 }
--- src/Movie.js
+++ src/Movie.js
@@ -8,9 +8,14 @@ function Movie({ movie }) {
             exit={{ opacity: 0, scale: 0 }}
             layout>
             <h2>{movie.title}</h2>
-            <img src={"https://image.tmdb.org/t/p/w500" + movie.backdrop_path} alt="" />
+            <img src={
+                "https://image.tmdb.org/t/p/w500" + movie.poster_path
+            } alt="" />
         </motion.div>
     )
 }
+
+
+//{this.popular.results[random].backdrop_path
 
 export default Movie;
